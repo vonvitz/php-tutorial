046ddfd833fbd5a31e3da687d905153102fa020d
vonvitz <81583583+vonvitz@users.noreply.github.com>
2026-04-19 18:23:15 +0800

    added for loop

---
+++ for_loop.php
@@ -0,0 +1,10 @@
+<?php
+
+    // For Loop = execute a block of code a specified number of times. It is used when you know in advance how many times you want to execute a block of code. The syntax of a for loop is as follows:
+
+    for($i = 0; $i < 10; $i++)
+    {
+        echo "The value of i is: {$i} <br>";
+    }
+
+?>
